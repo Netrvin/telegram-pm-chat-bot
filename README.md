42a6b21ceaf400db70b29a8afa771b3e88d99d02
Netrvin <131@moe.im>
2018-08-02 20:21:36 +0800

    v1.0.1
    默认关闭息发送提示
    添加Ping~Pong!
    收到表情时将自动提示发送者
    添加英文

--- README.md
+++ README.md
@@ -2,15 +2,18 @@
 Telegram 私聊机器人
 Telegram Private Message Chat Bot
 
-## 安装
+## 安装 (Installation)
 
-### 安装准备
-* 申请Telegram机器人，获取Token
+### 安装准备 (Preparation)
+* 创建Telegram机器人，获取Token
 * 一台外面的服务器，安装好Python 2和pip，并运行指令`pip install python-telegram-bot --upgrade`
 
-### 配置
+* Create a bot and get its token
+* Install Python 2 and pip, then run command `pip install python-telegram-bot --upgrade`
+
+### 配置 (Configuration)
 打开`config.json`并配置
-```
+```json
 {
     "Admin": 0,        # 管理员用户ID（数字ID）（可以先不设）
     "Token": "",       # 机器人Token
@@ -19,22 +22,57 @@ Telegram Private Message Chat Bot
 ```
 如果在前一步未设置管理员用户ID，第一个对机器人发送`/setadmin`的用户将成为管理员，之后可通过修改`config.json`修改管理员
 
-## 运行
+Open `config.json` and configure
+```json
+{
+    "Admin": 0,        # Admin ID (A digital ID)
+    "Token": "",       # Bot Token
+    "Lang": "en"       # Language Pack Name (Be careful! It's "en"!)
+}
+```
+If you didn't set admin's ID previously, the user who sends `/setadmin` to the bot firstly will become the admin. You can edit `config.json` to change admin later.
+
+## 运行 (Run)
 ```
 python main.py
 ```
 
-## 使用
+## 使用 (Usage)
 
-### 回复
+### 回复 (Reply)
 直接回复机器人转发过来的消息即可回复，支持文字、贴纸、图片、文件、音频和视频
 
-### 查询用户身份
+Reply directly to the message forwarded by the robot to reply. You can reply text, sticker, photo, file, audio, voice and video.
+
+### 查询用户身份 (Inquire sender identity)
 部分转发来的消息不便于查看发送者身份，可以通过回复该消息`/info`查询
 
-### 消息发送提示
+You can reply `/info` to the message which you want to get its sender's info more clearly.
+
+### 消息发送提示 (Message sending notification)
 向机器人发送指令`/togglenotification`可开启/关闭消息发送提示
 
 效果：
 * 对管理员：回复用户后，如无出错则不会提示“已回复”
 * 对用户：发送消息后，机器人不会回复“已收到”
+
+Send the command `/togglenotification` to the bot to enable/disable the message sending notification
+
+Effect:
+* For admin: After replying to the user, if there is no error, it will not prompt "replied"
+* For users: After sending a message, the bot will not reply "received"
+
+## 可用指令 (Available commands)
+| Command             | 用途                   |
+| :---                | :---                   |
+| /ping               | 确认机器人是否正在运行   |
+| /setadmin           | 设置当前用户为管理员     |
+| /togglenotification | 切换消息发送提示开启状态 |
+| /info               | 查询用户身份            |
+
+| Command             | Usage                                      |
+| :---                | :---                                       |
+| /ping               | Check if the bot is running                |
+| /setadmin           | Set the current user as admin              |
+| /togglenotification | Toggle message sending notification status |
+| /info               | Inquire sender identity                    |
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import threading
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-Version_Code = 'v1.0.0'
+Version_Code = 'v1.0.1' # 版本号
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
@@ -22,19 +22,19 @@ logging.basicConfig(level=logging.INFO,
 
 PATH = os.path.dirname(os.path.realpath(__file__)) + '/'
 
-CONFIG = json.loads(open(PATH + 'config.json', 'r').read())
+CONFIG = json.loads(open(PATH + 'config.json', 'r').read()) # 加载配置文件
 
-LANG = json.loads(open(PATH + 'lang/' + CONFIG['Lang'] + '.json').read())
+LANG = json.loads(open(PATH + 'lang/' + CONFIG['Lang'] + '.json').read()) # 加载语言文件
 
 MESSAGE_LOCK = False
 
-message_list = json.loads(open(PATH + 'data.json', 'r').read())
+message_list = json.loads(open(PATH + 'data.json', 'r').read()) # 加载消息数据
 
 PREFERENCE_LOCK = False
 
-preference_list = json.loads(open(PATH + 'preference.json','r').read())
+preference_list = json.loads(open(PATH + 'preference.json','r').read()) # 加载用户资料与设置
 
-def save_data():
+def save_data():    # 保存消息数据
     global MESSAGE_LOCK
     while MESSAGE_LOCK:
         time.sleep(0.05)
@@ -44,7 +44,7 @@ def save_data():
     f.close()
     MESSAGE_LOCK = False
 
-def save_preference():
+def save_preference():  # 保存用户资料与设置
     global PREFERENCE_LOCK
     while PREFERENCE_LOCK:
         time.sleep(0.05)
@@ -54,20 +54,20 @@ def save_preference():
     f.close()
     PREFERENCE_LOCK = False
 
-def save_config():
+def save_config():  # 保存配置
     f = open(PATH + 'config.json', 'w')
     f.write(json.dumps(CONFIG, indent=4))
     f.close()
 
-def init_user(user):
+def init_user(user):    #初始化用户
     global preference_list
-    if not preference_list.has_key(str(user.id)):
+    if not preference_list.has_key(str(user.id)):   # 如果用户是第一次使用Bot
         preference_list[str(user.id)]={}
-        preference_list[str(user.id)]['notification']=True
-        preference_list[str(user.id)]['name']=user.full_name
+        preference_list[str(user.id)]['notification']=False # 默认关闭消息发送提示
+        preference_list[str(user.id)]['name']=user.full_name    # 保存用户昵称
         threading.Thread(target=save_preference).start()
         return
-    if preference_list[str(user.id)]['name']!=user.full_name:
+    if preference_list[str(user.id)]['name']!=user.full_name:   #如果用户的昵称变了
         preference_list[str(user.id)]['name']=user.full_name
         threading.Thread(target=save_preference).start()
 
@@ -80,17 +80,18 @@ CONFIG['Username'] = '@' + me.username
 
 print 'Starting... (ID: ' + str(CONFIG['ID']) + ', Username: ' + CONFIG['Username'] + ')'
 
-def process_msg(bot, update):
+def process_msg(bot, update):   # 处理消息
     global message_list
     init_user(update.message.from_user)
-    if CONFIG['Admin'] == 0:
+    if CONFIG['Admin'] == 0:    # 如果未设置管理员
         bot.send_message(chat_id=update.message.from_user.id,text=LANG['please_setup_first'])
         return
-    if update.message.from_user.id == CONFIG['Admin']:
-        if update.message.reply_to_message != None:
-            if message_list.has_key(str(update.message.reply_to_message.message_id)):
+    if update.message.from_user.id == CONFIG['Admin']:  # 如果是管理员发送的消息
+        if update.message.reply_to_message != None: # 如果未回复消息
+            if message_list.has_key(str(update.message.reply_to_message.message_id)):   # 如果消息数据存在
                 msg = update.message
                 sender_id = message_list[str(update.message.reply_to_message.message_id)]['sender_id']
+                # 匿名转发
                 try:
                     if msg.audio != None:
                         bot.send_audio(chat_id=sender_id,audio=msg.audio, caption=msg.caption)
@@ -111,27 +112,29 @@ def process_msg(bot, update):
                         return
                 except Exception as e:
                     if e.message == "Forbidden: bot was blocked by the user":
-                        bot.send_message(chat_id=CONFIG['Admin'],text=LANG['blocked_alert'])
+                        bot.send_message(chat_id=CONFIG['Admin'],text=LANG['blocked_alert'])    # Bot被停用
                     else:
                         bot.send_message(chat_id=CONFIG['Admin'],text=LANG['reply_message_failed'])
                     return
-                if preference_list[str(update.message.from_user.id)]['notification']:
+                if preference_list[str(update.message.from_user.id)]['notification']:   # 如果启用消息发送提示
                     bot.send_message(chat_id=update.message.chat_id,text=LANG['reply_message_sent'] % (preference_list[str(sender_id)]['name'],str(sender_id)),parse_mode=telegram.ParseMode.MARKDOWN)
             else:
                 bot.send_message(chat_id=CONFIG['Admin'],text=LANG['reply_to_message_no_data'])
         else:
             bot.send_message(chat_id=CONFIG['Admin'],text=LANG['reply_to_no_message'])
-    else:
-        fwd_msg = bot.forward_message(chat_id=CONFIG['Admin'], from_chat_id=update.message.chat_id, message_id=update.message.message_id)
-        if preference_list[str(update.message.from_user.id)]['notification']:
+    else:   # 如果不是管理员发送的消息
+        fwd_msg = bot.forward_message(chat_id=CONFIG['Admin'], from_chat_id=update.message.chat_id, message_id=update.message.message_id)   # 转发消息
+        if fwd_msg.sticker != None: # 如果是贴纸，则发送发送者身份提示
+            bot.send_message(chat_id=CONFIG['Admin'],text=LANG['info_data'] % (update.message.from_user.full_name,str(update.message.from_user.id)),parse_mode=telegram.ParseMode.MARKDOWN,reply_to_message_id=fwd_msg.message_id)
+        if preference_list[str(update.message.from_user.id)]['notification']:   # 如果启用消息发送提示
             bot.send_message(chat_id=update.message.from_user.id,text=LANG['message_received_notification'])
         message_list[str(fwd_msg.message_id)]={}
         message_list[str(fwd_msg.message_id)]['sender_id']=update.message.from_user.id
-        threading.Thread(target=save_data).start()
+        threading.Thread(target=save_data).start()  # 保存消息数据
     pass
 
 
-def process_command(bot, update):
+def process_command(bot, update):   #处理指令
     init_user(update.message.from_user)
     id=update.message.from_user.id
     global CONFIG
@@ -147,15 +150,15 @@ def process_command(bot, update):
                          + '\nhttps://github.com/Netrvin/telegram-pm-chat-bot'
                          )
         return
-    elif command[0] == 'setadmin':
-        if CONFIG['Admin']==0:
+    elif command[0] == 'setadmin':  # 设置管理员
+        if CONFIG['Admin']==0:  # 判断管理员是否未设置
             CONFIG['Admin']=int(update.message.from_user.id)
             save_config()
             bot.send_message(chat_id=update.message.chat_id,text=LANG['set_admin_successful'])
         else:
             bot.send_message(chat_id=update.message.chat_id,text=LANG['set_admin_failed'])
         return
-    elif command[0] == 'togglenotification':
+    elif command[0] == 'togglenotification':    # 切换消息发送提示开启状态
         global preference_list
         preference_list[str(id)]['notification']=(preference_list[str(id)]['notification'] == False)
         threading.Thread(target=save_preference).start()
@@ -163,25 +166,27 @@ def process_command(bot, update):
             bot.send_message(chat_id=update.message.chat_id,text=LANG['togglenotification_on'])
         else:
             bot.send_message(chat_id=update.message.chat_id,text=LANG['togglenotification_off'])
-    elif command[0] == 'info':
+    elif command[0] == 'info':  # 发送者信息
         if (update.message.from_user.id == CONFIG['Admin']) and (update.message.chat_id == CONFIG['Admin']):
             if update.message.reply_to_message != None:
                 if message_list.has_key(str(update.message.reply_to_message.message_id)):
                     sender_id=message_list[str(update.message.reply_to_message.message_id)]['sender_id']
-                    bot.send_message(chat_id=update.message.chat_id,text=LANG['info_data'] % (preference_list[str(sender_id)]['name'],str(sender_id)),parse_mode=telegram.ParseMode.MARKDOWN)
+                    bot.send_message(chat_id=update.message.chat_id,text=LANG['info_data'] % (preference_list[str(sender_id)]['name'],str(sender_id)),parse_mode=telegram.ParseMode.MARKDOWN,reply_to_message_id=update.message.reply_to_message.message_id)
                 else:
                     bot.send_message(chat_id=update.message.chat_id,text=LANG['reply_to_message_no_data'])
+    elif command[0] == 'ping':  # Ping~Pong!
+        bot.send_message(chat_id=update.message.chat_id,text='Pong!')
 
-dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.all & telegram.ext.Filters.private & (~ telegram.ext.Filters.command) & (~ telegram.ext.Filters.status_update), process_msg))
+# 添加Handle
+dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.all & telegram.ext.Filters.private & (~ telegram.ext.Filters.command) & (~ telegram.ext.Filters.status_update), process_msg))   # 处理消息
 
-dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.command & telegram.ext.Filters.private,
-                                                   process_command))
+dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.command & telegram.ext.Filters.private,process_command))    # 处理指令
 
-updater.start_polling()
+updater.start_polling() # 开始轮询
 print 'Started'
 updater.idle()
 print 'Stopping...'
-save_data()
-save_preference()
+save_data() # 保存消息数据
+save_preference()   # 保存用户资料与设置
 print 'Data saved.'
 print 'Stopped.'
